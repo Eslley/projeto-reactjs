e4fd5fe5ec95e8ecd44beee09641cc39ad33f0b6
eslleyrubem <rubemeslley@gmail.com>
2022-10-21 16:42:50 -0300

    Styling and CSS Basics

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,14 +1,21 @@
 import React from 'react'
+import './person.css';
 
-function Person({person, index}) {
-  return (
-    <div>
-        <p>{index}</p>
-        <p>I am {person.name}</p>
-        <p>I am {person.years} years old</p>
-        <p>I know {person.skill}</p>
-    </div>
-  )
+function Person({ person, index }) {
+
+    const nameStyle = {
+        fontSize: '30px',
+        color: 'blue'
+    }
+
+    return (
+        <div>
+            <p className='id'>{index}</p>
+            <p style={nameStyle}>I am {person.name}</p>
+            <p>I am {person.years} years old</p>
+            <p>I know {person.skill}</p>
+        </div>
+    )
 }
 
 export default Person
