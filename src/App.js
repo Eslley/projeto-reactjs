9554b37bd624cbcf5f49db8409f424f40e58a45b
eslleyrubem <rubemeslley@gmail.com>
2022-10-21 14:16:49 -0300

    Creating Class Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import MyComponent from './components/Greet'
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
       <MyComponent />
+      <Welcome />
     </div>
   );
 }
