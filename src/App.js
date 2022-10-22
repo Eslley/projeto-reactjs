736b76cd19ddf6ee6f73194e26b30795d6360597
eslleyrubem <rubemeslley@gmail.com>
2022-10-22 03:07:15 -0300

    Using Portals

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import Table from './components/Table';
 import PureComp from './components/PureComponent';
 import RefsDemo from './components/RefsDemo';
 import FRParentInput from './components/FRParentInput';
+import PortalDemo from './components/PortalDemo';
 
 function App() {
   return (
@@ -49,7 +50,9 @@ function App() {
 
       {/* <RefsDemo /> */}
 
-      <FRParentInput />
+      {/* <FRParentInput /> */}
+
+      <PortalDemo />
 
     </div>
   );
